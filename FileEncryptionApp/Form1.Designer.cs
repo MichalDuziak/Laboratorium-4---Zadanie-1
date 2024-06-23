d55c20f65246921db7917ea5be1339c59b8a9ada
Michal <Michal@MICHA>
2024-06-24 01:08:45 +0200

    Dodaj pliki projektów.

---
+++ FileEncryptionApp/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace FileEncryptionApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnSelectFiles;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.TextBox txtSelectedFiles;
+        private System.Windows.Forms.ComboBox cmbEncryptionMethod;
+        private System.Windows.Forms.Label lblSelectedFiles;
+        private System.Windows.Forms.Label lblEncryptionMethod;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnSelectFiles = new System.Windows.Forms.Button();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.txtSelectedFiles = new System.Windows.Forms.TextBox();
+            this.cmbEncryptionMethod = new System.Windows.Forms.ComboBox();
+            this.lblSelectedFiles = new System.Windows.Forms.Label();
+            this.lblEncryptionMethod = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSelectFiles
+            // 
+            this.btnSelectFiles.Location = new System.Drawing.Point(12, 12);
+            this.btnSelectFiles.Name = "btnSelectFiles";
+            this.btnSelectFiles.Size = new System.Drawing.Size(100, 23);
+            this.btnSelectFiles.TabIndex = 0;
+            this.btnSelectFiles.Text = "Select Files";
+            this.btnSelectFiles.UseVisualStyleBackColor = true;
+            this.btnSelectFiles.Click += new System.EventHandler(this.btnSelectFiles_Click);
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(118, 12);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(100, 23);
+            this.btnSelectFolder.TabIndex = 1;
+            this.btnSelectFolder.Text = "Select Folder";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(12, 70);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(100, 23);
+            this.btnEncrypt.TabIndex = 2;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(118, 70);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(100, 23);
+            this.btnDecrypt.TabIndex = 3;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(224, 70);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(100, 23);
+            this.btnUpload.TabIndex = 4;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // txtSelectedFiles
+            // 
+            this.txtSelectedFiles.Location = new System.Drawing.Point(12, 41);
+            this.txtSelectedFiles.Name = "txtSelectedFiles";
+            this.txtSelectedFiles.Size = new System.Drawing.Size(312, 20);
+            this.txtSelectedFiles.TabIndex = 5;
+            // 
+            // cmbEncryptionMethod
+            // 
+            this.cmbEncryptionMethod.FormattingEnabled = true;
+            this.cmbEncryptionMethod.Items.AddRange(new object[] {
+            "AES",
+            "DES",
+            "RSA"});
+            this.cmbEncryptionMethod.Location = new System.Drawing.Point(118, 99);
+            this.cmbEncryptionMethod.Name = "cmbEncryptionMethod";
+            this.cmbEncryptionMethod.Size = new System.Drawing.Size(206, 21);
+            this.cmbEncryptionMethod.TabIndex = 6;
+            // 
+            // lblSelectedFiles
+            // 
+            this.lblSelectedFiles.AutoSize = true;
+            this.lblSelectedFiles.Location = new System.Drawing.Point(329, 44);
+            this.lblSelectedFiles.Name = "lblSelectedFiles";
+            this.lblSelectedFiles.Size = new System.Drawing.Size(76, 13);
+            this.lblSelectedFiles.TabIndex = 7;
+            this.lblSelectedFiles.Text = "Selected Files:";
+            // 
+            // lblEncryptionMethod
+            // 
+            this.lblEncryptionMethod.AutoSize = true;
+            this.lblEncryptionMethod.Location = new System.Drawing.Point(12, 102);
+            this.lblEncryptionMethod.Name = "lblEncryptionMethod";
+            this.lblEncryptionMethod.Size = new System.Drawing.Size(99, 13);
+            this.lblEncryptionMethod.TabIndex = 8;
+            this.lblEncryptionMethod.Text = "Encryption Method:";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(417, 146);
+            this.Controls.Add(this.lblEncryptionMethod);
+            this.Controls.Add(this.lblSelectedFiles);
+            this.Controls.Add(this.cmbEncryptionMethod);
+            this.Controls.Add(this.txtSelectedFiles);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Controls.Add(this.btnSelectFiles);
+            this.Name = "Form1";
+            this.Text = "File Encryption App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ FileEncryptionApp/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace FileEncryptionApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFiles_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtSelectedFiles.Text = string.Join(", ", openFileDialog.FileNames);
+            }
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtSelectedFiles.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            var files = txtSelectedFiles.Text.Split(new[] { ", " }, StringSplitOptions.None);
+            foreach (var file in files)
+            {
+                Task.Run(() => EncryptFile(file));
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            var files = txtSelectedFiles.Text.Split(new[] { ", " }, StringSplitOptions.None);
+            foreach (var file in files)
+            {
+                Task.Run(() => DecryptFile(file));
+            }
+        }
+
+        private void EncryptFile(string filePath)
+        {
+            try
+            {
+                string encryptedFilePath = filePath.EndsWith(".txt") ? filePath.Substring(0, filePath.Length - 4) + ".enc" : filePath;
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Key = Convert.FromBase64String("1Sp7hV2QWcIbtf5N2AiiZ1OfZlnlmzu0r3rwX4o/ABw=");
+                    aes.IV = Convert.FromBase64String("PcgsK5UV5zqirB7MxJD+Mw==");
+                    //klucze wygenerowałem poprzez osobny projekt
+                    ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                    using (FileStream fsEncrypted = new FileStream(encryptedFilePath, FileMode.Create))
+                    {
+                        using (CryptoStream cs = new CryptoStream(fsEncrypted, encryptor, CryptoStreamMode.Write))
+                        {
+                            using (FileStream fsIn = new FileStream(filePath, FileMode.Open))
+                            {
+                                int data;
+                                while ((data = fsIn.ReadByte()) != -1)
+                                {
+                                    cs.WriteByte((byte)data);
+                                }
+                            }
+                        }
+                    }
+                }
+                MessageBox.Show($"File {filePath} encrypted successfully.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error encrypting file {filePath}: {ex.Message}");
+            }
+        }
+
+        private void DecryptFile(string filePath)
+        {
+            try
+            {
+                string decryptedFilePath = filePath.EndsWith(".enc") ? filePath.Substring(0, filePath.Length - 4) + "-Decrypted.txt" : filePath;
+
+
+                //aby działało deszyfrowanie trzeba było usunąć plik, który się szyfrowało (konflikt nazw plików)
+                //poradziłem sobie w taki sposób, aby usuwać z niego 8 znaków od końca czyli .enc oraz .txt  teraz test.txt.enc będzie miał nazwę testEncrypted.txt
+
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Key = Convert.FromBase64String("1Sp7hV2QWcIbtf5N2AiiZ1OfZlnlmzu0r3rwX4o/ABw=");
+                    aes.IV = Convert.FromBase64String("PcgsK5UV5zqirB7MxJD+Mw==");
+                    //klucze wygenerowałem poprzez osobny projekt
+                    ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                    using (FileStream fsIn = new FileStream(filePath, FileMode.Open))
+                    {
+                        using (CryptoStream cs = new CryptoStream(fsIn, decryptor, CryptoStreamMode.Read))
+                        {
+                            using (FileStream fsOut = new FileStream(decryptedFilePath, FileMode.Create))
+                            {
+                                int data;
+                                while ((data = cs.ReadByte()) != -1)
+                                {
+                                    fsOut.WriteByte((byte)data);
+                                }
+                            }
+                        }
+                    }
+                }
+                MessageBox.Show($"File {filePath} decrypted successfully.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error decrypting file {filePath}: {ex.Message}");
+            }
+        }
+
+        private async void btnUpload_Click(object sender, EventArgs e)
+        {
+            var files = txtSelectedFiles.Text.Split(new[] { ", " }, StringSplitOptions.None);
+            foreach (var file in files)
+            {
+                await Task.Run(() => UploadFile(file));
+            }
+        }
+
+        private void UploadFile(string filePath)
+        {
+            MessageBox.Show($"File {filePath} uploaded successfully.");
+        }
+
+        private void SaveConfig(EncryptionConfig config, string filePath)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            using (Stream stream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            {
+                formatter.Serialize(stream, config);
+            }
+        }
+
+        private EncryptionConfig LoadConfig(string filePath)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            using (Stream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                return (EncryptionConfig)formatter.Deserialize(stream);
+            }
+        }
+    }
+}
